70028d1a7a3123fb4c7f9e36af10b92b1482d64f
anojbsunk333 <anojbsunk500@gmail.com>
2026-02-17 10:48:09 +0545

    clock

--- BasicJavascript/clockusing/script.js
+++ BasicJavascript/clockusing/script.js
@@ -1,6 +1,4 @@
-let a = new Date()
-let h = a.getHours()
-let m = a.getMinutes()
-let s = a.getSeconds()
-let d = a.getDate()
-console.log(h, m, s, d)
+setInterval(() => {
+    let d = new Date()
+    time.innerHTML = d
+}, 1000)
